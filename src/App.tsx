a216fd35a346b103e89382e1683b7b6489f01066
Vinh Pham <vinh@vinh.dev>
2024-01-21 20:20:09 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,177 +1,302 @@
-import { Authenticated, Refine } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
+import React from "react";
+import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
+import { RefineKbarProvider } from "@refinedev/kbar";
 import {
-  ErrorComponent,
-  ThemedLayoutV2,
-  ThemedSiderV2,
-  ThemedTitleV2,
-  useNotificationProvider,
+    notificationProvider,
+    ThemedLayoutV2,
+    ErrorComponent,
 } from "@refinedev/antd";
+import routerProvider, {
+    CatchAllNavigate,
+    NavigateToResource,
+    UnsavedChangesNotifier,
+    DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import {
+    ShoppingOutlined,
+    UsergroupAddOutlined,
+    ShopOutlined,
+    StarOutlined,
+    DashboardOutlined,
+} from "@ant-design/icons";
+import jsonServerDataProvider from "@refinedev/simple-rest";
+import { authProvider } from "./authProvider";
+
+import "dayjs/locale/de";
+
+import { DashboardPage } from "./pages/dashboard";
+import { OrderList, OrderShow } from "./pages/orders";
+import { AuthPage } from "./pages/auth";
+import { UserList, UserShow } from "./pages/users";
+import {
+    CourierList,
+    CourierShow,
+    CourierCreate,
+    CourierEdit,
+} from "./pages/couriers";
+import { ProductList } from "./pages/products";
+import { StoreCreate, StoreEdit, StoreList } from "./pages/stores";
+import { CategoryList } from "./pages/categories";
+import { ReviewsList } from "./pages/reviews";
+import { useTranslation } from "react-i18next";
+import { Header, Title, OffLayoutArea } from "./components";
+import { BikeWhiteIcon, PizzaIcon } from "./components/icons";
+import { ConfigProvider } from "./context";
+import { useAutoLoginForDemo } from "./hooks";
+
 import "@refinedev/antd/dist/reset.css";
 
-import routerBindings, {
-  CatchAllNavigate,
-  DocumentTitleHandler,
-  NavigateToResource,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
-import { App as AntdApp } from "antd";
-import { useTranslation } from "react-i18next";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import { authProvider } from "./authProvider";
-import { AppIcon } from "./components/app-icon";
-import { Header } from "./components/header";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import {
-  BlogPostCreate,
-  BlogPostEdit,
-  BlogPostList,
-  BlogPostShow,
-} from "./pages/blog-posts";
-import {
-  CategoryCreate,
-  CategoryEdit,
-  CategoryList,
-  CategoryShow,
-} from "./pages/categories";
-import { ForgotPassword } from "./pages/forgotPassword";
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
-import Playground from "./components/playground";
+const App: React.FC = () => {
+    // This hook is used to automatically login the user.
+    // We use this hook to skip the login page and demonstrate the application more quickly.
+    const { loading } = useAutoLoginForDemo();
 
-function App() {
-  const { t, i18n } = useTranslation();
+    const API_URL = "https://api.finefoods.refine.dev";
+    const dataProvider = jsonServerDataProvider(API_URL);
 
-  const i18nProvider = {
-    translate: (key: string, params: object) => t(key, params),
-    changeLocale: (lang: string) => i18n.changeLanguage(lang),
-    getLocale: () => i18n.language,
-  };
+    const { t, i18n } = useTranslation();
 
-  return (
-    <BrowserRouter>
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-                notificationProvider={useNotificationProvider}
-                authProvider={authProvider}
-                i18nProvider={i18nProvider}
-                routerProvider={routerBindings}
-                resources={[
-                  {
-                    name: "blog_posts",
-                    list: "/blog-posts",
-                    create: "/blog-posts/create",
-                    edit: "/blog-posts/edit/:id",
-                    show: "/blog-posts/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                  {
-                    name: "categories",
-                    list: "/categories",
-                    create: "/categories/create",
-                    edit: "/categories/edit/:id",
-                    show: "/categories/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                  {
-                    name: "playground",
-                    list: "/playground",
-                    meta: {
-                      label: "Playground",
-                    },
-                  }
+    const i18nProvider = {
+        translate: (key: string, params: object) => t(key, params),
+        changeLocale: (lang: string) => i18n.changeLanguage(lang),
+        getLocale: () => i18n.language,
+    };
 
-                ]}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  useNewQueryKeys: true,
-                  projectId: "Kkc734-uo0zbs-8IjICr",
-                }}
-              >
-                <Routes>
-                  <Route
-                    element={
-                      <Authenticated
-                        key="authenticated-inner"
-                        fallback={<CatchAllNavigate to="/login" />}
-                      >
-                        <ThemedLayoutV2
-                          Header={() => <Header sticky />}
-                          Sider={(props) => <ThemedSiderV2 {...props} fixed />}
-                          Title={({ collapsed }) => (
-                            <ThemedTitleV2
-                              collapsed={collapsed}
-                              text="refine playground"
-                              icon={<AppIcon />}
-                            />
-                          )}
-                        >
-                          <Outlet />
-                        </ThemedLayoutV2>
-                      </Authenticated>
-                    }
-                  >
-                    <Route
-                      index
-                      element={<NavigateToResource resource="blog_posts" />}
-                    />
-                    <Route path="/blog-posts">
-                      <Route index element={<BlogPostList />} />
-                      <Route path="create" element={<BlogPostCreate />} />
-                      <Route path="edit/:id" element={<BlogPostEdit />} />
-                      <Route path="show/:id" element={<BlogPostShow />} />
-                    </Route>
-                    <Route path="/categories">
-                      <Route index element={<CategoryList />} />
-                      <Route path="create" element={<CategoryCreate />} />
-                      <Route path="edit/:id" element={<CategoryEdit />} />
-                      <Route path="show/:id" element={<CategoryShow />} />
-                    </Route>
-                    <Route path="/playground" element={<Playground />} />
-                    <Route path="*" element={<ErrorComponent />} />
-                  </Route>
-                  <Route
-                    element={
-                      <Authenticated
-                        key="authenticated-outer"
-                        fallback={<Outlet />}
-                      >
-                        <NavigateToResource />
-                      </Authenticated>
-                    }
-                  >
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route
-                      path="/forgot-password"
-                      element={<ForgotPassword />}
-                    />
-                  </Route>
-                </Routes>
+    if (loading) {
+        return null;
+    }
 
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
-}
+    return (
+        <BrowserRouter>
+            <ConfigProvider>
+                <RefineKbarProvider>
+                    <Refine
+                        routerProvider={routerProvider}
+                        dataProvider={dataProvider}
+                        authProvider={authProvider}
+                        i18nProvider={i18nProvider}
+                        options={{
+                            syncWithLocation: true,
+                            warnWhenUnsavedChanges: true,
+                        }}
+                        notificationProvider={notificationProvider}
+                        resources={[
+                            {
+                                name: "dashboard",
+                                list: "/",
+                                meta: {
+                                    label: "Dashboard",
+                                    icon: <DashboardOutlined />,
+                                },
+                            },
+                            {
+                                name: "orders",
+                                list: "/orders",
+                                show: "/orders/show/:id",
+                                meta: {
+                                    icon: <ShoppingOutlined />,
+                                },
+                            },
+                            {
+                                name: "users",
+                                list: "/users",
+                                show: "/users/show/:id",
+                                meta: {
+                                    icon: <UsergroupAddOutlined />,
+                                },
+                            },
+                            {
+                                name: "products",
+                                list: "/products",
+                                meta: {
+                                    icon: <PizzaIcon />,
+                                },
+                            },
+                            {
+                                name: "stores",
+                                list: "/stores",
+                                create: "/stores/create",
+                                edit: "/stores/edit/:id",
+                                meta: {
+                                    icon: <ShopOutlined />,
+                                },
+                            },
+                            {
+                                name: "categories",
+                                list: "/categories",
+                            },
+                            {
+                                name: "couriers",
+                                list: "/couriers",
+                                create: "/couriers/create",
+                                edit: "/couriers/edit/:id",
+                                show: "/couriers/show/:id",
+                                meta: {
+                                    icon: <BikeWhiteIcon />,
+                                },
+                            },
+                            {
+                                name: "reviews",
+                                list: "/reviews",
+                                meta: {
+                                    icon: <StarOutlined />,
+                                },
+                            },
+                        ]}
+                    >
+                        <Routes>
+                            <Route
+                                element={
+                                    <Authenticated
+                                        key="authenticated-routes"
+                                        fallback={
+                                            <CatchAllNavigate to="/login" />
+                                        }
+                                    >
+                                        <ThemedLayoutV2
+                                            Header={Header}
+                                            Title={Title}
+                                            OffLayoutArea={OffLayoutArea}
+                                        >
+                                            <Outlet />
+                                        </ThemedLayoutV2>
+                                    </Authenticated>
+                                }
+                            >
+                                <Route index element={<DashboardPage />} />
+
+                                <Route path="/orders">
+                                    <Route index element={<OrderList />} />
+                                    <Route
+                                        path="show/:id"
+                                        element={<OrderShow />}
+                                    />
+                                </Route>
+
+                                <Route path="/users">
+                                    <Route index element={<UserList />} />
+                                    <Route
+                                        path="show/:id"
+                                        element={<UserShow />}
+                                    />
+                                </Route>
+
+                                <Route
+                                    path="/products"
+                                    element={<ProductList />}
+                                />
+
+                                <Route path="/stores">
+                                    <Route index element={<StoreList />} />
+                                    <Route
+                                        path="create"
+                                        element={<StoreCreate />}
+                                    />
+                                    <Route
+                                        path="edit/:id"
+                                        element={<StoreEdit />}
+                                    />
+                                </Route>
+
+                                <Route
+                                    path="/categories"
+                                    element={<CategoryList />}
+                                />
+
+                                <Route path="/couriers">
+                                    <Route index element={<CourierList />} />
+                                    <Route
+                                        path="create"
+                                        element={<CourierCreate />}
+                                    />
+                                    <Route
+                                        path="edit/:id"
+                                        element={<CourierEdit />}
+                                    />
+                                    <Route
+                                        path="show/:id"
+                                        element={<CourierShow />}
+                                    />
+                                </Route>
+
+                                <Route
+                                    path="/reviews"
+                                    element={<ReviewsList />}
+                                />
+                            </Route>
+
+                            <Route
+                                element={
+                                    <Authenticated
+                                        key="auth-pages"
+                                        fallback={<Outlet />}
+                                    >
+                                        <NavigateToResource resource="dashboard" />
+                                    </Authenticated>
+                                }
+                            >
+                                <Route
+                                    path="/login"
+                                    element={
+                                        <AuthPage
+                                            type="login"
+                                            formProps={{
+                                                initialValues: {
+                                                    email: "demo@refine.dev",
+                                                    password: "demodemo",
+                                                },
+                                            }}
+                                        />
+                                    }
+                                />
+                                <Route
+                                    path="/register"
+                                    element={
+                                        <AuthPage
+                                            type="register"
+                                            formProps={{
+                                                initialValues: {
+                                                    email: "demo@refine.dev",
+                                                    password: "demodemo",
+                                                },
+                                            }}
+                                        />
+                                    }
+                                />
+                                <Route
+                                    path="/forgot-password"
+                                    element={<AuthPage type="forgotPassword" />}
+                                />
+                                <Route
+                                    path="/update-password"
+                                    element={<AuthPage type="updatePassword" />}
+                                />
+                            </Route>
+
+                            <Route
+                                element={
+                                    <Authenticated key="catch-all">
+                                        <ThemedLayoutV2
+                                            Header={Header}
+                                            Title={Title}
+                                            OffLayoutArea={OffLayoutArea}
+                                        >
+                                            <Outlet />
+                                        </ThemedLayoutV2>
+                                    </Authenticated>
+                                }
+                            >
+                                <Route path="*" element={<ErrorComponent />} />
+                            </Route>
+                        </Routes>
+                        <UnsavedChangesNotifier />
+                        <DocumentTitleHandler />
+                    </Refine>
+                </RefineKbarProvider>
+            </ConfigProvider>
+        </BrowserRouter>
+    );
+};
 
 export default App;
--- src/authProvider.ts
+++ src/authProvider.ts
@@ -1,59 +1,93 @@
-import { AuthBindings } from "@refinedev/core";
+import { AuthProvider } from "@refinedev/core";
+import { notification } from "antd";
+import { disableAutoLogin, enableAutoLogin } from "./hooks";
 
 export const TOKEN_KEY = "refine-auth";
 
-export const authProvider: AuthBindings = {
-  login: async ({ username, email, password }) => {
-    if ((username || email) && password) {
-      localStorage.setItem(TOKEN_KEY, username);
-      return {
-        success: true,
-        redirectTo: "/",
-      };
-    }
+export const authProvider: AuthProvider = {
+    login: async ({ email, password }) => {
+        enableAutoLogin();
+        localStorage.setItem(TOKEN_KEY, `${email}-${password}`);
+        return {
+            success: true,
+            redirectTo: "/",
+        };
+    },
+    register: async ({ email, password }) => {
+        try {
+            await authProvider.login({ email, password });
+            return {
+                success: true,
+            };
+        } catch (error) {
+            return {
+                success: false,
+                error: {
+                    message: "Register failed",
+                    name: "Invalid email or password",
+                },
+            };
+        }
+    },
+    updatePassword: async () => {
+        notification.success({
+            message: "Updated Password",
+            description: "Password updated successfully",
+        });
+        return {
+            success: true,
+        };
+    },
+    forgotPassword: async ({ email }) => {
+        notification.success({
+            message: "Reset Password",
+            description: `Reset password link sent to "${email}"`,
+        });
+        return {
+            success: true,
+        };
+    },
+    logout: async () => {
+        disableAutoLogin();
+        localStorage.removeItem(TOKEN_KEY);
+        return {
+            success: true,
+            redirectTo: "/login",
+        };
+    },
+    onError: async (error) => {
+        console.error(error);
+        return { error };
+    },
+    check: async () => {
+        const token = localStorage.getItem(TOKEN_KEY);
+        if (token) {
+            return {
+                authenticated: true,
+            };
+        }
 
-    return {
-      success: false,
-      error: {
-        name: "LoginError",
-        message: "Invalid username or password",
-      },
-    };
-  },
-  logout: async () => {
-    localStorage.removeItem(TOKEN_KEY);
-    return {
-      success: true,
-      redirectTo: "/login",
-    };
-  },
-  check: async () => {
-    const token = localStorage.getItem(TOKEN_KEY);
-    if (token) {
-      return {
-        authenticated: true,
-      };
-    }
+        return {
+            authenticated: false,
+            error: {
+                message: "Check failed",
+                name: "Token not found",
+            },
+            logout: true,
+            redirectTo: "/login",
+        };
+    },
+    getPermissions: async () => null,
+    getIdentity: async () => {
+        const token = localStorage.getItem(TOKEN_KEY);
+        if (!token) {
+            return null;
+        }
 
-    return {
-      authenticated: false,
-      redirectTo: "/login",
-    };
-  },
-  getPermissions: async () => null,
-  getIdentity: async () => {
-    const token = localStorage.getItem(TOKEN_KEY);
-    if (token) {
-      return {
-        id: 1,
-        name: "John Doe",
-        avatar: "https://i.pravatar.cc/300",
-      };
-    }
-    return null;
-  },
-  onError: async (error) => {
-    console.error(error);
-    return { error };
-  },
+        return {
+            id: 1,
+            name: "James Sullivan",
+            avatar: "https://i.pravatar.cc/150",
+        };
+    },
 };
